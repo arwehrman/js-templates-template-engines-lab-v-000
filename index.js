be82739fae2e9bdb5a3f70d537edbd3f39be6c0c
Amanda Wehrman <arwehrman@gmail.com>
2018-02-01 21:36:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function createPost() {
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
   var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
-  
+
   var title = document.getElementById("postTitle").value;
   var author = document.getElementById("postAuthor").value;
   var post = document.getElementById("postText").value;
